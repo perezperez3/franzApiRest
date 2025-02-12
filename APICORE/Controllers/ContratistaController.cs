5d1421142fbc71115aa6b95fdeeaf77820b4ab34
perezperez3 <frances.perez@mecolabora.com.pe>
2025-02-12 09:35:07 -0500

    se agrega el proyecto

---
+++ APICORE/Controllers/ContratistaController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using APICORE.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace APICORE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContratistaController : ControllerBase
+    {
+        private readonly string cadenaSQL;
+
+        public ContratistaController(IConfiguration config) {
+            cadenaSQL = config.GetConnectionString("CadenaSQL");
+        }
+
+        [HttpGet]
+        [Route("Lista")]
+
+        public IActionResult Lista() {
+            List<Contratista> lista = new List<Contratista>();
+
+            try
+            {
+                using (var conexion = new SqlConnection(cadenaSQL)) { 
+
+                    conexion.Open();
+                    var cmd = new SqlCommand("sp_listar_contratista", conexion);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    using (var rd = cmd.ExecuteReader()) { 
+                        
+                        while (rd.Read())
+                        {
+                            lista.Add(new Contratista
+                            {
+                                iIdContratista = Convert.ToInt32(rd["iIdContratista"]),
+                                vRuc = rd["vRuc"].ToString(),
+                                vRazonSocial = rd["vRazonSocial"].ToString(),
+                                vRepresentante = rd["vRepresentante"].ToString(),
+                                vCorreo = rd["vCorreo"].ToString(),
+                                vTelefono = rd["vTelefono"].ToString(),
+                                iEstado = Convert.ToInt32(rd["iEstado"]),
+                                fLatitud = Convert.ToDecimal(rd["fLatitud"]),
+                                fLongitud = Convert.ToDecimal(rd["fLongitud"]),
+
+                            });
+                        }
+                    }
+                
+                }
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = "ok", Response = lista });
+
+            }
+            catch (Exception error)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, new { mensaje = error.Message, Response = lista });
+            }
+
+        
+        }
+    }
+}
---
+++ APICORE/Models/Contratista.cs
@@ -0,0 +1,16 @@
+﻿namespace APICORE.Models
+{
+    public class Contratista
+    {
+        public int iIdContratista { get; set; }
+        public string vRuc { get; set; }
+        public string vRazonSocial { get; set; }
+        public string vRepresentante { get; set; }
+        public string vCorreo { get; set; }
+        public string vTelefono { get; set; }
+        public int iEstado { get; set; }
+        public DateTime dfechaRegistro { get; set; }
+        public decimal fLatitud { get; set; }
+        public decimal fLongitud { get; set; }
+    }
+}
